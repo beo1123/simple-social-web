b09bd7e2667f4154c62f30adc5e769c1b4d92d46
vbao964@gmail.com <vbao964@gmail.com>
2025-09-05 20:39:15 +0700

    test husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.css"
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1 className="text-3xl text-red-500 font-bold underline">
+          Hello world!
+        </h1>
         <Image
           className={styles.logo}
           src="/next.svg"
